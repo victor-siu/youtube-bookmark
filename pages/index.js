0cb835a4652d3f7bd660dd2153af1355634f1955
R Tsang <richard.tsang9641@gmail.com>
2020-09-28 09:55:55 +0800

    Added new dependencies and scripts

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,45 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Posts from './posts'
+// This function gets called at build time
+  export async function getStaticProps() {
+    // Call an external API endpoint to get videos
+    const res = await fetch('https://api.fighter.hk/youtube/videos.php')
+    const videos = await res.json()
+  
+    // By returning { props: videos }, the Blog component
+    // will receive `videos` as a prop at build time
+    return {
+      props:{videos,}
+    }
+  }
 
-export default function Home() {
+export default function Home({videos}) {
   
   
   
   return (
     <div className={styles.container}>
       <Head>
-        <title>Youtube Hub</title>
+        <title>Youtube Bookmark</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Youtube Hub!
+          Welcome to Youtube Bookmark!
         </h1>
 
         <div className={styles.flex}>
-          <Posts/>
+        <h1>videos</h1>
+      {(videos && videos.videos.length > 0) ? 
+      videos.videos.map(({vid,url,columnName})=> <iframe key={vid} width="560" height="315"
+      src={url.replace("watch?v=","embed/")}  frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen"
+      mozallowfullscreen="mozallowfullscreen" 
+      msallowfullscreen="msallowfullscreen" 
+      oallowfullscreen="oallowfullscreen" 
+      webkitallowfullscreen="webkitallowfullscreen">
+      </iframe> ): <p><strong>Have nothing!!</strong></p>}
+    
         </div>
       </main>
 
